792038aaa9bc67d328c86c78059b5b0b889a6f10
LianaCR <90052155+LianaCR@users.noreply.github.com>
2021-11-17 11:21:09 -0300

    Atualização Model Aluno

--- ICV.WebUIMVC/Models/AlunoModel.cs
+++ ICV.WebUIMVC/Models/AlunoModel.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -10,7 +11,7 @@ namespace ICV.WebUIMVC.Models
     {
         
         [Required]
-        public string Cpf { get; set; }
+        public string CpfAluno { get; set; }
 
         [Required]
         public string DataNascimento { get; set; }
@@ -21,30 +22,103 @@ namespace ICV.WebUIMVC.Models
         [Required]
         public int FKIdColaborador { get; set; }
 
+        string dataAtual = DateTime.Now.ToString();
 
         public override AlunoModel Buscar(int id)
         {
-            throw new NotImplementedException();
+            SqlConnection conn = new SqlConnection(ConecteDb.Connect());
+            conn.Open();
+
+            string sql = @"Select * From TblAluno where IdAluno =" + id;
+
+            SqlCommand cmd = new SqlCommand(sql, conn);
+            SqlDataReader dr = cmd.ExecuteReader();
+            AlunoModel Aluno = new AlunoModel();
+
+            if (dr.Read())
+            {
+                Aluno.id = Convert.ToInt32(dr["IdAluno"]);
+                Aluno.Nome = dr["NomeAluno"].ToString();
+                Aluno.CpfAluno = dr["CpfAluno"].ToString();
+                Aluno.DataNascimento = dr["DataNascimentoAluno"].ToString();
+                Aluno.Email = dr["EmailAluno"].ToString();
+                Aluno.Telefone = dr["TelefoneAluno"].ToString();
+                Aluno.Status = (Status) Convert.ToInt32(dr["StatusAluno"]);
+                Aluno.DataCadastro = dr["DataCadastroAluno"].ToString();
+                Aluno.FKIdTurma = Convert.ToInt32(dr["FkIdTurma"]);
+                Aluno.FKIdColaborador = Convert.ToInt32(dr["FkIdColaborador"]);
+            }
+
+            return Aluno;
         }
 
         public override List<AlunoModel> Buscar()
         {
-            throw new NotImplementedException();
+            SqlConnection conn = new SqlConnection(ConecteDb.Connect());
+            conn.Open();
+
+            string sql = @"Select * From TblAluno";
+
+            SqlCommand cmd = new SqlCommand( sql,conn);
+            SqlDataReader dr = cmd.ExecuteReader();
+            List<AlunoModel> listaObj = new List<AlunoModel>();
+
+            if (dr.Read())
+            {
+                AlunoModel Aluno = new AlunoModel();
+
+                Aluno.id = Convert.ToInt32(dr["IdAluno"]);
+                Aluno.Nome = dr["NomeAluno"].ToString();
+                Aluno.CpfAluno = dr["CpfAluno"].ToString();
+                Aluno.DataNascimento = dr["DataNascimentoAluno"].ToString();
+                Aluno.Email = dr["EmailAluno"].ToString();
+                Aluno.Telefone = dr["TelefoneAluno"].ToString();
+                Aluno.Status = (Status)Convert.ToInt32(dr["StatusAluno"]);
+                Aluno.DataCadastro = dr["DataCadastroAluno"].ToString();
+                Aluno.FKIdTurma = Convert.ToInt32(dr["FkIdTurma"]);
+                Aluno.FKIdColaborador = Convert.ToInt32(dr["FkIdColaborador"]);
+
+                listaObj.Add(Aluno);
+            }
+
+            return listaObj;
+            
         }
 
         public override void Cadastrar(AlunoModel objeto)
         {
-            throw new NotImplementedException();
+            SqlConnection conn = new SqlConnection(ConecteDb.Connect());
+            conn.Open();
+
+            string sql = @"Insert into TblAluno values('"+objeto.Nome+ "','" + objeto.CpfAluno + "','" + objeto.DataNascimento + "','" + objeto.Email + "','" + objeto.Telefone + "','" + objeto.Status + "','" + dataAtual + "','" + objeto.FKIdTurma + "','" + objeto.FKIdColaborador + "')";
+
+            SqlCommand cmd = new SqlCommand(sql,conn);
+            cmd.ExecuteNonQuery();
+            conn.Close();
         }
 
         public override void Editar(AlunoModel objeto, int id)
         {
-            throw new NotImplementedException();
+            SqlConnection con = new SqlConnection(ConecteDb.Connect());
+            con.Open();
+
+            string sql = "Update TblAluno Set NomeAluno='" + objeto.Nome + "', CpfAluno='" + objeto.CpfAluno + "', DataNascimentoAluno='" + objeto.DataNascimento + "', EmailAluno='" + objeto.Email + "', TelefoneAluno='" + objeto.Telefone + "',  StatusAluno='" + objeto.Status + "',  where IdAluno=" + objeto.id;
+            
+            SqlCommand cmd = new SqlCommand(sql, con);
+            cmd.ExecuteNonQuery();
+            con.Close();
         }
 
         public override void Remover(int id)
         {
-            throw new NotImplementedException();
+            SqlConnection con = new SqlConnection(ConecteDb.Connect());
+            con.Open();
+
+            string sql = "Delete From TblAluno Where IdAluno=" + id + "";
+
+            SqlCommand cmd = new SqlCommand(sql, con);
+            cmd.ExecuteNonQuery();
+            con.Close();
         }
     }
 }
