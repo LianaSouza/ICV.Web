eb98301a82ea0a046dba4738fc6ae006505e6f45
PABLO BORGES RODRIGUES <pablo.brodrigues2@senacsp.edu.br>
2021-11-22 10:39:18 -0300

    Pablo Cadastrar aluno e colaborador views e  controllers

---
+++ ICV.WebUIMVC/Controllers/ColaboradorController.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ICV.WebUIMVC.Controllers
+{
+    public class ColaboradorController : Controller
+    {
+        // GET: Colaborador
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        // GET: Colaborador/Details/5
+        public ActionResult Detalhes(int id)
+        {
+            return View();
+        }
+
+        // GET: Colaborador/Create
+        public ActionResult Cadastrar()
+        {
+            return View();
+        }
+
+        // POST: Colaborador/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Cadastrar(IFormCollection collection)
+        {
+            try
+            {
+                // TODO: Add insert logic here
+
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Colaborador/Edit/5
+        public ActionResult Editar(int id)
+        {
+            return View();
+        }
+
+        // POST: Colaborador/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Editar(int id, IFormCollection collection)
+        {
+            try
+            {
+                // TODO: Add update logic here
+
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Colaborador/Delete/5
+        public ActionResult Deletar(int id)
+        {
+            return View();
+        }
+
+        // POST: Colaborador/Delete/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Deletar(int id, IFormCollection collection)
+        {
+            try
+            {
+                // TODO: Add delete logic here
+
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
