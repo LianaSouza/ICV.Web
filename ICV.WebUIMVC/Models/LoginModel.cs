f4c0fa09f0361756717944d81f5cdd70a6a1d2df
LianaCR <90052155+LianaCR@users.noreply.github.com>
2021-11-24 10:57:30 -0300

    Criação telo login, resolução problema compilação.

---
+++ ICV.WebUIMVC/Models/LoginModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ICV.WebUIMVC.Models
+{
+    public class LoginModel
+    {
+        [Required]
+        public string Email { get; set; }
+
+        [Required]
+        public string Senha { get; set; }
+       
+    }
+}
