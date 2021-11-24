d9bed7a34e923f3be7c6c1532da3c53e759baca1
RUAN BRAGA DE MOURA <ruan.bmoura@senacsp.edu.br>
2021-11-24 11:39:17 -0300

    Ruan Braga_EntradaDoacao controller e metodos criados

---
+++ ICV.WebUIMVC/Controllers/DoacaoEntradaController.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using ICV.WebUIMVC.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ICV.WebUIMVC.Controllers
+{
+    public class DoacaoEntradaController : Controller
+    {
+        // GET: DoacaoEntrada
+        public ActionResult Index()
+        {
+            return View(new DoacaoEntradaModel().Buscar());
+        }
+
+        // GET: DoacaoEntrada/Details/5
+        public ActionResult Detalhes(int id)
+        {
+            return View(new DoacaoEntradaModel().Buscar(id));
+        }
+
+        // GET: DoacaoEntrada/Create
+        public ActionResult Cadastrar()
+        {
+            return View();
+        }
+
+        // POST: DoacaoEntrada/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Cadastrar(DoacaoEntradaModel objeto)
+        {
+            try
+            {
+                // TODO: Add insert logic here
+
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: DoacaoEntrada/Edit/5
+        public ActionResult Editar(int id)
+        {
+            return View(new DoacaoEntradaModel().Buscar(id));
+        }
+
+        // POST: DoacaoEntrada/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Editar(int id, DoacaoEntradaModel objeto)
+        {
+            try
+            {
+                // TODO: Add update logic here
+                new DoacaoEntradaModel().Editar(objeto, id);
+
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: DoacaoEntrada/Delete/5
+        public ActionResult Excluir(int id)
+        {
+            return View(new DoacaoEntradaModel().Buscar(id));
+        }
+
+        // POST: DoacaoEntrada/Delete/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Excluir(int id, DoacaoEntradaModel objeto)
+        {
+            try
+            {
+                // TODO: Add delete logic here
+                new DoacaoEntradaModel().Remover(id);
+
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
