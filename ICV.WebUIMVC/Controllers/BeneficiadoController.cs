5b1590fd1c6682a4f04740cc1aa839c3b6c96aa7
RUAN BRAGA DE MOURA <ruan.bmoura@senacsp.edu.br>
2021-11-24 10:38:03 -0300

    Ruan Braga_Finalizado ControllerBeneficiado

--- ICV.WebUIMVC/Controllers/BeneficiadoController.cs
+++ ICV.WebUIMVC/Controllers/BeneficiadoController.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using ICV.WebUIMVC.Models;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
@@ -12,13 +13,13 @@ namespace ICV.WebUIMVC.Controllers
         // GET: Beneficiado
         public ActionResult Index()
         {
-            return View();
+            return View(new BeneficiadoModel().Buscar());
         }
 
         // GET: Beneficiado/Details/5
         public ActionResult Detalhes(int id)
         {
-            return View();
+            return View(new BeneficiadoModel().Buscar(id));
         }
 
         // GET: Beneficiado/Create
@@ -30,7 +31,7 @@ namespace ICV.WebUIMVC.Controllers
         // POST: Beneficiado/Create
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public ActionResult Cadastrar(IFormCollection collection)
+        public ActionResult Cadastrar(BeneficiadoModel collection)
         {
             try
             {
@@ -47,17 +48,18 @@ namespace ICV.WebUIMVC.Controllers
         // GET: Beneficiado/Edit/5
         public ActionResult Editar(int id)
         {
-            return View();
+            return View(new BeneficiadoModel().Buscar(id));
         }
 
         // POST: Beneficiado/Edit/5
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public ActionResult Editar(int id, IFormCollection collection)
+        public ActionResult Editar(int id, BeneficiadoModel collection)
         {
             try
             {
                 // TODO: Add update logic here
+                new BeneficiadoModel().Editar(collection, id);
 
                 return RedirectToAction(nameof(Index));
             }
@@ -70,17 +72,18 @@ namespace ICV.WebUIMVC.Controllers
         // GET: Beneficiado/Delete/5
         public ActionResult Excluir(int id)
         {
-            return View();
+            return View(new BeneficiadoModel().Buscar(id));
         }
 
         // POST: Beneficiado/Delete/5
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public ActionResult Excluir(int id, IFormCollection collection)
+        public ActionResult Excluir(int id, BeneficiadoModel collection)
         {
             try
             {
                 // TODO: Add delete logic here
+                new BeneficiadoModel().Remover(id);
 
                 return RedirectToAction(nameof(Index));
             }
